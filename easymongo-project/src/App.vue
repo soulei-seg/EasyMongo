2f787f42e93cf44510a620d75f1f171a3d42ace6
soulei-seg <souleimane.seghir@ynov.com>
2020-01-14 10:48:03 +0100

    Création de la première vue

--- easymongo-project/src/App.vue
+++ easymongo-project/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title msg="Easy Mongo Project"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Accueil.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
